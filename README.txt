4e24727c9a21f839181fd0cc26b5f5097ced7266
Sheyla <sheylvsz01@gmail.com>
2022-05-08 19:03:01 -0400

    add optional params

---
+++ README.txt
@@ -0,0 +1,9 @@
+This vars will let you change de form of the tree:
+rdt_factor_angle1, rdt_factor_angle2, rdt_factor_branch1, rdt_factor_branch2.
+
+They start with 1/2 but I recommend change rdt_factor_branch1 and rdt_factor_branch2 to random.uniform(0.8, 0.9).
+
+Important: 
+    if you use a very small reduction factor, you should change the min_branch_lenght too.
+
+The colors are selected randomly
--- fractal_tree.py
+++ fractal_tree.py
@@ -1,59 +1,63 @@
 import turtle
+from typing import Tuple
 import random
 
+ #random.uniform(0.8, 0.9)
 
-#Global variables
-RDT_FACTOR_ANGLE1 = 1/2
-RDT_FACTOR_ANGLE2 = 1/2
-RDT_PORCENT_BRANCH1 = random.uniform(0.8, 0.9)
-RDT_PORCENT_BRANCH2 = random.uniform(0.8, 0.9)
+def pick_color() -> Tuple[int, int, int]:
+    """Create a color with the rgb agreement"""
+    color = (0, 0, 0)
+    while color == (0, 0, 0):
+        color = tuple(random.randint(0, 255) for _ in range(3))
+    return color
 
-#Create a color with the rgb agreement
-def pick_colors():
-    color = [random.randrange(0, 256) for _ in range(3)]
-    r, g, b = color
-    if r == 0 and g == 0 and b == 0:
-        pick_colors()
-    else:
-        return r, g, b
 
-#Inicial settings to turtle module
-window = turtle.Screen()
-window.bgcolor("black")
-turtle.colormode(255)
-pen = turtle.Turtle()
+def pain_tree(
+    branch_lenght: float, pen: turtle.Turtle, min_branch_lenght: float, angle: float, 
+    rdt_factor_angle1=1 / 2, rdt_factor_angle2= 1 / 2, rdt_factor_branch1= 1 / 2, rdt_factor_branch2= 1 / 2
+) -> None:
+    """
+    Paints the fractral tree
 
-#Personalization of the pen 
-pen.speed(0)
-pen.left(90)
-pen.penup()
-pen.setpos(0, -250)
-pen.pendown()
-pen.hideturtle()
-pen.pensize(1)
+    Parameters
+    ----------
+    branch_lengh : float
+        Lenght of the branch
+    pen : turtle.Turtle
+        Pen to paint
+    min_branch_lenght : int
+        Minimal lenght of the branch
+    angle : int
+        Angle of the branch
+    """
 
-#Painting tree algorithm
-   # params: branch_lenght -> nicial lenght of a branch
-   #         pen -> Turtle() to pain on the module
-   #         min_branch_lenght -> stop condition for recurtion
-   #         angle -> inicial angle of the branches
-
-def pain_tree(branch_lenght: int, pen: turtle.Turtle, min_branch_lenght: int, angle: int):
-
-    if (branch_lenght < min_branch_lenght):     
+    if branch_lenght < min_branch_lenght:
         return
 
-    pen.color(pick_colors())
+    pen.color(pick_color())
     pen.forward(branch_lenght)
-    pen.left(angle * RDT_FACTOR_ANGLE1)
-    pain_tree(branch_lenght * RDT_PORCENT_BRANCH1, pen, min_branch_lenght, angle)
+    pen.left(angle * rdt_factor_angle1)
+    pain_tree(branch_lenght * rdt_factor_branch1, pen, min_branch_lenght, angle)
     pen.right(angle)
-    pain_tree(branch_lenght * RDT_PORCENT_BRANCH2, pen, min_branch_lenght, angle)
-    pen.left(angle * RDT_FACTOR_ANGLE2)
+    pain_tree(branch_lenght * rdt_factor_angle2, pen, min_branch_lenght, angle)
+    pen.left(angle * rdt_factor_branch2)
     pen.backward(branch_lenght)
 
 
-#Execute
-pain_tree(100, pen, 40, 60)
+# Initial settings to turtle module
+window = turtle.Screen()
+window.bgcolor("black")
+turtle.colormode(255)
 
+# Pen setup
+my_pen = turtle.Turtle()
+my_pen.speed(0)
+my_pen.left(90)
+my_pen.penup()
+my_pen.setpos(0, -250)
+my_pen.pendown()
+my_pen.hideturtle()
+my_pen.pensize(2)
+
+pain_tree(300, my_pen, 1, 90)
 window.exitonclick()
